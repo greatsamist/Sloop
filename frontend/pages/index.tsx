621b9ab37baa31f9f6a2b8b5a70a5a07620a7262
Samuel Ojo <ifegbenga@gmail.com>
2022-09-19 00:09:43 +0100

    fix deploy

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Fragment } from "react";
 import {
   Accountability,
   HowItWorksSection,
   IndexHero,
   WellTailored,
 } from "@components";
+import { Fragment } from "react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
